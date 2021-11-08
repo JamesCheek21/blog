34e5a210bcabea74e93293303033bb1439d4bafe
JamesCheek21 <jamesfrancis80@aol.com>
2021-11-08 10:41:14 +0000

    Initial Setup  +  Blog Set Up w/ Navigation and Constraints

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/app.css">
+    <title>My Blog</title>
+</head>
+<body>
+    <article>
+        <h1><a href="/posts/my-first-post">My First Post</a></h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sit amet cursus justo. Integer varius ullamcorper consequat. Suspendisse ornare risus at elit rhoncus pulvinar. Morbi vel elit pharetra, porta tellus vitae, pretium leo. Quisque dictum orci vehicula porttitor finibus. Praesent eu quam varius, ultricies tortor at, hendrerit libero. Vestibulum semper lacus a fermentum congue. In hendrerit faucibus est, ut feugiat nunc semper vel. Pellentesque laoreet felis et lacus ullamcorper mattis. Aliquam ac sapien at ligula aliquet tincidunt. Mauris malesuada mauris in tellus tempus sodales. Duis et elit massa. Duis lacinia nulla vitae arcu viverra, quis gravida massa imperdiet. Cras sit amet posuere leo, at interdum turpis. Suspendisse imperdiet dapibus lacus porttitor consectetur. Nulla mattis urna at ipsum tincidunt semper. Aliquam facilisis placerat eros non finibus. Fusce rutrum varius augue a consectetur. Vivamus varius libero at blandit consequat. Sed dui lacus, consequat nec iaculis vel, porta vel eros. Fusce ornare porttitor diam, in dignissim ante congue eu. Pellentesque ut sodales lacus. Suspendisse viverra augue in justo luctus, et congue risus ullamcorper. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+        </p>
+    </article>
+
+    <article>
+    <h1><a href="/posts/my-second-post">My Second Post</a></h1>
+        <p>
+        dolor sit amet, consectetur adipiscing elit. Aliquam sit amet cursus justo. Integer varius ullamcorper consequat. Suspendisse ornare risus at elit rhoncus pulvinar. Morbi vel elit pharetra, porta tellus vitae, pretium leo. Quisque dictum orci vehicula porttitor finibus. Praesent eu quam varius, ultricies tortor at, hendrerit libero. Vestibulum semper lacus a fermentum congue. In hendrerit faucibus est, ut feugiat nunc semper vel. Pellentesque laoreet felis et lacus ullamcorper mattis. Aliquam ac sapien at ligula aliquet tincidunt. Mauris malesuada mauris in tellus tempus sodales. Duis et elit massa. Duis lacinia nulla vitae arcu viverra, quis gravida massa imperdiet. Cras sit amet posuere leo, at interdum turpis. Suspendisse imperdiet dapibus lacus porttitor consectetur. Nulla mattis urna at ipsum tincidunt semper. Aliquam facilisis placerat eros non finibus. Fusce rutrum varius augue a consectetur. Vivamus varius libero at blandit consequat. Sed dui lacus, consequat nec iaculis vel, porta vel eros. Fusce ornare porttitor diam, in dignissim ante congue eu. Pellentesque ut sodales lacus. Suspendisse viverra augue in justo luctus, et congue risus ullamcorper. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+        </p>
+    </article>
+    <article>
+        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
+        <p>
+        dolor sit amet, consectetur adipiscing elit. Aliquam sit amet cursus justo. Integer varius ullamcorper consequat. Suspendisse ornare risus at elit rhoncus pulvinar. Morbi vel elit pharetra, porta tellus vitae, pretium leo. Quisque dictum orci vehicula porttitor finibus. Praesent eu quam varius, ultricies tortor at, hendrerit libero. Vestibulum semper lacus a fermentum congue. In hendrerit faucibus est, ut feugiat nunc semper vel. Pellentesque laoreet felis et lacus ullamcorper mattis. Aliquam ac sapien at ligula aliquet tincidunt. Mauris malesuada mauris in tellus tempus sodales. Duis et elit massa. Duis lacinia nulla vitae arcu viverra, quis gravida massa imperdiet. Cras sit amet posuere leo, at interdum turpis. Suspendisse imperdiet dapibus lacus porttitor consectetur. Nulla mattis urna at ipsum tincidunt semper. Aliquam facilisis placerat eros non finibus. Fusce rutrum varius augue a consectetur. Vivamus varius libero at blandit consequat. Sed dui lacus, consequat nec iaculis vel, porta vel eros. Fusce ornare porttitor diam, in dignissim ante congue eu. Pellentesque ut sodales lacus. Suspendisse viverra augue in justo luctus, et congue risus ullamcorper. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+        </p>
+    </article>
+</body>
+</html>
