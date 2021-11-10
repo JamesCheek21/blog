c7693f754949fbe062b3e2f27090c90a6510d8d9
JamesCheek21 <jamesfrancis80@aol.com>
2021-11-10 17:01:36 +0000

    Query constraints

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -21,6 +21,17 @@ class Post extends Model
                 ->where('title', 'like', '%' . $search . '%')
                 ->orWhere('body', 'like', '%' . $search . '%');
         });
+
+        $query->when(isset($filters['category']) ? $filters['category'] : false, function ($query, $category) {
+            // $query->whereExists(function($query) use ($category) {
+            //     $query->from('categories')
+            //     ->whereColumn('categories.id', 'posts.category_id')
+            //     ->where('categories.slug', $category);
+            // });
+            $query->whereHas('category', function($query) use ($category) {
+                $query->where('slug', $category);
+            });
+        });
     }
 
     public function category()
