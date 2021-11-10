fd1918b3eefcd31fe881de70b9b34a9a29814ecf
JamesCheek21 <jamesfrancis80@aol.com>
2021-11-10 16:05:33 +0000

    Search - Clean Way

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -30,15 +30,15 @@
                     {!! $post->excerpt !!}
             </div>
 
-            <footer class="flex justify-between items-center mt-8">
-                <div class="flex items-center text-sm">
+            <footer class="flex justify-between items-center mt-8 ">
+                <div class="flex items-center text-sm w-52">
                     <img src="/images/lary-avatar.svg" alt="Lary avatar">
                     <div class="ml-3">
                         <h5 class="font-bold">{{ $post->author->name}}</h5>
                     </div>
                 </div>
 
-                <div>
+                <div class='flex'>
                     <a href="/post/{{ $post->slug }}"
                         class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8"
                     >Read More</a>
