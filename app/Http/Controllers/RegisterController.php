9dbf825eb97e674881754e24e8aceebdf44e1f80
JamesCheek21 <jamesfrancis80@aol.com>
2021-11-11 14:47:05 +0000

    Login and Logout

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -18,8 +18,10 @@ class RegisterController extends Controller
             'password' => 'required|min:8',
         ]);
 
-        User::create($attributes);
+        $user = User::create($attributes);
 
-        return redirect("/");
+        auth()->login($user);
+
+        return redirect("/")->with('success', 'Your account has been created.');
     }
 }
