a339e3dbb64ee94bcf287531543de5b9ede4c611
JamesCheek21 <jamesfrancis80@aol.com>
2021-11-11 15:04:21 +0000

    Login Page

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -2,10 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class SessionsController extends Controller
 {
+    public function create()
+    {
+        return view('sessions.create');
+    }
+    public function store()
+    {
+        $attributes = request()->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if(auth()->attempt($attributes)){
+            session()->regenerate();
+            return redirect('/')->with('success', 'Welcome back!');
+        }
+
+        throw ValidationException::withMessages([
+            'email' => 'Your provided credentials could not be verified'
+        ]);
+        // return back()
+        //     ->withInput()
+        //     ->withErrors(['email' => 'Your provided credentials could not be verified']);
+
+    }
     public function destroy()
     {
         auth()->logout();
