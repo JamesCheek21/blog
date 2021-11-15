7ad358347ded199206c68555064c391602879096
JamesCheek21 <jamesfrancis80@aol.com>
2021-11-15 15:28:01 +0000

    Authorisation

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -34,11 +34,13 @@
                         <x-slot name="trigger">
                             <button class="text-xs font-bold uppercase">Welcome, {{ auth()->user()->name }}</button>
                         </x-slot>
-                        <x-dropdown-item href="/admin/posts" :active="request()->is('admin/posts')">All Posts</x-dropdown-item>
-                        <x-dropdown-item href="/admin/posts/create" :active="request()->is('admin/posts/create')">New Post</x-dropdown-item>
+                        @can('admin')
+                            <x-dropdown-item href="/admin/posts" :active="request()->is('admin/posts')">All Posts</x-dropdown-item>
+                            <x-dropdown-item href="/admin/posts/create" :active="request()->is('admin/posts/create')">New Post</x-dropdown-item>
+                        @endcan
                         <x-dropdown-item href="#" x-data="{}" @click.prevent="document.querySelector('#logout-form').submit()">Log Out</x-dropdown-item>
                     </x-dropdown>
-                    <form id="logout-form" method="POST" action="/logout" class="text-xs font-semibold text-blue-500 ml-6">
+                    <form id="logout-form" method="POST" action="/logout" class="text-xs font-semibold text-blue-500 ml-6 hidden">
                             @csrf
                             <button type="submit">Logout</button>
                     </form>
