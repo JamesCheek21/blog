b38d7dbc694f4728fedf12286da28163baa5882f
JamesCheek21 <jamesfrancis80@aol.com>
2021-11-15 14:46:46 +0000

    Edit and Delete Posts plus Category Dropdown fix

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -1,3 +1,6 @@
+{{-- @php
+    ddd(request()->input("category") == $categories[0]->slug)
+@endphp --}}
 <x-dropdown>
     <x-slot name="trigger">
         <button class="py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w-32 text-left lg:inline-flex flex">
@@ -8,15 +11,19 @@
     </x-slot>
     <x-dropdown-item
         href="/?{{ http_build_query(request()->except('category', 'page')) }}"
-        :active="request()->routeIs('home')">
+        :active="request()->input() == null">
+        {{-- @php
+            ddd(request()->input() == null)
+        @endphp --}}
         All
     </x-dropdown-item>
     @foreach ($categories as $category)
         <x-dropdown-item
         href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category', 'page')) }}"
-        :active='request()->is("categories/{$category->slug}")'
+        :active="request()->input('category') == $category->slug"
         >
             {{ucwords($category->name)}}
         </x-dropdown-item>
     @endforeach
 </x-dropdown>
+
--- resources/views/components/form/textarea.blade.php
+++ resources/views/components/form/textarea.blade.php
@@ -2,10 +2,10 @@
 
 <x-form.field>
     <x-form.label name="{{ $name }}"/>
-    <textarea class="border border-gray-400 p-2 w-full"
+    <textarea class="border border-gray-200 p-2 w-full"
         name="{{ $name }}"
         id="{{ $name }}"
-    >{{ old($name) }}</textarea>
+    >{{ $slot }}</textarea>
 
     <x-form.error name="{{$name}}" />
 </x-form.field>
